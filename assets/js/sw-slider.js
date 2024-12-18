6093afb5c3c618086ceb03f32a41b8eae90c040f
Yurbus <frankins636@gmail.com>
2024-12-18 14:28:45 +0200

    fff

--- assets/js/sw-slider.js
+++ assets/js/sw-slider.js
@@ -45,7 +45,7 @@ var swiper = new Swiper(".mySwiper2", {
         890: {
             slidesPerView: 3,
         },
-        1024: {
+        1600: {
             slidesPerView: 4,
         },
     },
@@ -53,7 +53,7 @@ var swiper = new Swiper(".mySwiper2", {
 var swiper = new Swiper(".mySwiper3", {
     cssMode: true,
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 5,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -66,48 +66,9 @@ var swiper = new Swiper(".mySwiper3", {
     breakpoints: {
         580: {
             slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        890: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-        },
-    },
-});
-var swiper = new Swiper(".mySwiper4", {
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    // grid: {
-    //     rows: 2,
-    // },
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    mousewheel: false,
-    keyboard: true,
-    breakpoints: {
-        580: {
-            slidesPerView: 2,
-            slidesPerGroup: 1,
         },
         960: {
-            slidesPerView: 3,
-            slidesPerGroup: 2,
-            
-        },
-        1024: {
             slidesPerView: 4,
-            slidesPerGroup: 4,
-        
         },
     },
 });
